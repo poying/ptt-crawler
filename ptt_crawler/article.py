488743edfe969aa2a999835e8c2549eb33a528cd
shunyi <M157q.tw@gmail.com>
2015-11-22 10:31:07 +0800

    Return None if ariticle don't have 'time' meta data

--- ptt_crawler/article.py
+++ ptt_crawler/article.py
@@ -36,7 +36,7 @@ class Article:
     @property
     def time(self):
         time = self.get_meta_data("time")
-        return parseTime(time)
+        return parseTime(time) if time else None
 
     @property
     def type(self):
